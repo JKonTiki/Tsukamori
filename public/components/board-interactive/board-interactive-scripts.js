6b6749d458921e310923e723b9431c554527fe5e
Jeremy Fryd <fryd21@gmail.com>
2017-05-01 16:12:56 -0700

    sustain synths already playing, add attack and release

--- public/components/board-interactive/board-interactive-scripts.js
+++ public/components/board-interactive/board-interactive-scripts.js
@@ -8,8 +8,8 @@ exports.mount = function(){
   const BOARD_WIDTH = 3000;
   const BOARD_HEIGHT = 3000;
   const BRUSH_RADIUS = 60;
-  const COL_COUNT = 50;
-  const ROW_COUNT = 50;
+  const COL_COUNT = 15;
+  const ROW_COUNT = 15;
   const PXLS_PER_COL = Math.floor(BOARD_WIDTH / COL_COUNT);
   const PXLS_PER_ROW = Math.floor(BOARD_WIDTH / ROW_COUNT);
   // this is the number of pxl rows we checkPt per Row
--- public/general/scripts/audio-synthesis.js
+++ public/general/scripts/audio-synthesis.js
@@ -1,8 +1,8 @@
 var errors = require('./../../general/scripts/errors');
 
-const TOTAL_DURATION = 15;
+const TOTAL_DURATION = 10;
 const FREQ_MIN = 30;
-const FREQ_MAX = 2000;
+const FREQ_MAX = 800;
 
 var audioContext = null;
 var synths = null;
@@ -37,16 +37,40 @@ exports.translateData = function(colNum, rowNum, data){
   // console.log(synths);
   let now = audioContext.currentTime + .01;
   let usedRows = getUsedRows(data);
+  let synthsPlaying = {};
   for (let colKey in data){
     colKey = parseInt(colKey);
     // reset synths to play again on next column
-    synths = setSynths(rowNum, usedRows);
+    synths = setSynths(rowNum, usedRows, synthsPlaying);
     for (let rowKey in data[colKey]){
-      synths[rowKey].gain.gain.setValueAtTime(now + (colKey * timeInterval), .1);
-      synths[rowKey].gain.gain.exponentialRampToValueAtTime(now + (colKey * timeInterval) + (colKey * timeInterval)/2, .4);
-      synths[rowKey].gain.gain.exponentialRampToValueAtTime(now + (colKey * timeInterval) + (colKey * timeInterval), .1);
-      synths[rowKey].oscillator.start(now + (colKey * timeInterval))
-      synths[rowKey].oscillator.stop(now + (colKey * timeInterval) + timeInterval)
+      rowKey = parseInt(rowKey);
+      let alreadyPlaying = false;
+      let continuesPlaying = false;
+      if (data[colKey - 1]) {
+        if (data[colKey - 1][rowKey]) {
+          alreadyPlaying = true;
+        }
+      }
+      if (data[colKey + 1]) {
+        if (data[colKey + 1][rowKey]) {
+          continuesPlaying = true;
+        }
+      }
+      if (!alreadyPlaying) {
+        synths[rowKey].gain.gain.setValueAtTime(.001, now + (colKey * timeInterval));
+        // attack
+        synths[rowKey].gain.gain.exponentialRampToValueAtTime(.2, now + (colKey * timeInterval) + timeInterval * .3);
+        synths[rowKey].oscillator.start(now + (colKey * timeInterval));
+        synthsPlaying[rowKey] = synths[rowKey];
+      }
+      if (!continuesPlaying) {
+        // sustain
+        synths[rowKey].gain.gain.exponentialRampToValueAtTime(.1, now + (colKey * timeInterval) + timeInterval * .6);
+        // release
+        synths[rowKey].gain.gain.exponentialRampToValueAtTime(.001, now + (colKey * timeInterval) + timeInterval);
+        synths[rowKey].oscillator.stop(now + (colKey * timeInterval) + timeInterval);
+        synthsPlaying[rowKey] = false;
+      }
     }
   }
   synths = null;
@@ -69,17 +93,17 @@ var getUsedRows = function(data){
   return usedRows;
 }
 
-var setSynths = function(rowNum, usedRows){
-  let synths = {};
+var setSynths = function(rowNum, usedRows, synthsPlaying){
+  let synths = synthsPlaying || {};
   for (var i = 0; i < rowNum; i++) {
-    if (usedRows.includes(i)) {
+    if (usedRows.includes(i) && !synthsPlaying[i]) {
       let frequency = parseFloat(FREQ_MIN + (freqInterval * i)).toFixed(3);
       //create synth's gain
       let gain = audioContext.createGain();
       gain.connect(audioContext.destination);
       //create synth's oscillator
       let oscillator = audioContext.createOscillator();
-      oscillator.type = 'sine';
+      oscillator.type = 'sawtooth';
       oscillator.frequency.value = frequency;
       oscillator.connect(gain);
       // set to synth object
